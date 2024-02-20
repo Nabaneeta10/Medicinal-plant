fc258f3fa66da48c5ad6314f4b3e19d3f8da5f77
Nabaneeta10 <nabaneetamohanta10@gmail.com>
2024-02-20 19:55:15 +0530

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
 
 let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
+let listItemDom = document.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
